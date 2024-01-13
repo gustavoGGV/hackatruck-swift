64a2565796e992785614e1452253532ce71ada7a
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-01-13 13:15:31 -0300

    Update idade.swift

--- idade.swift
+++ idade.swift
@@ -3,9 +3,9 @@ idade = 3
 
 if idade < 3 {
     print("Um bebê")
-} else if idade >= 3 && idade <= 10 {
+} else if idade >= 3 && idade <= 12 {
     print("Uma criança")
-} else if idade >= 11 && idade <= 17 {
+} else if idade >= 13 && idade <= 17 {
     print("Um adolescente")
 } else {
     print("Um adulto")
