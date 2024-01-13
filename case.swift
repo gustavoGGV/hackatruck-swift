88133eb4e01a40382f985d0646a3f3e44fbe75a0
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-01-13 13:22:50 -0300

    Create case.swift

---
+++ case.swift
@@ -0,0 +1,28 @@
+var nota:Int
+nota = 10
+
+switch nota {
+case 2: 
+    print("O usuário solicitou uma nota de 2 reais.")
+    
+case 5:
+    print("O usuário solicitou uma nota de 5 reais.")
+    
+case 10:
+    print("O usuário solicitou uma nota de 10 reais.")
+    
+case 20:
+    print("O usuário solicitou uma nota de 20 reais.")
+    
+case 50:
+    print("O usuário solicitou uma nota de 50 reais.")
+    
+case 100:
+    print("O usuário solicitou uma nota de 100 reais.")
+    
+case 200:
+    print("O usuário solicitou uma nota de 200 reais.")
+    
+default:
+    print("A nota solicita não existe.")
+}
