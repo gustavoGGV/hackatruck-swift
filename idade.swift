13b4d342f6ded8beba28e84a11546fd067e01e40
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-01-13 13:14:41 -0300

    Create idade.swift

---
+++ idade.swift
@@ -0,0 +1,12 @@
+var idade: Int
+idade = 3
+
+if idade < 3 {
+    print("Um bebê")
+} else if idade >= 3 && idade <= 10 {
+    print("Uma criança")
+} else if idade >= 11 && idade <= 17 {
+    print("Um adolescente")
+} else {
+    print("Um adulto")
+}
