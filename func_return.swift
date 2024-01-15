f0fa625429ecd7d79fac2559afe41a68bf282dc0
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-01-15 13:48:06 -0300

    Create func_return.swift

---
+++ func_return.swift
@@ -0,0 +1,11 @@
+func somaInt(n:Int, n2:Int)->String{
+    return "O resultado da soma destes números é \((n) + (n2))."
+}
+
+print(somaInt(n:10, n2:20))
+
+func divInt(n:Double, n2:Double)->String{
+    return "O resultado da divisão destes números é \((n) / (n2))."
+}
+
+print(divInt(n:20, n2:3))
